4deacb415b6e76bbe8aae4d930cd47a677d96b44
Ismael <muhammadismaelosman@gmail.com>
2024-03-25 22:22:29 +0800

    more stuff

--- main.go
+++ main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello!")
+	var nameOne string = "Mario"
+	var nameTwo = "Luigi"
+	var nameThree string
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	// another way to create a variable
+	nameFour := "yoshi"
+
+	fmt.Println(nameFour)
 }
